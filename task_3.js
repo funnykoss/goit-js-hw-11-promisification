c6b5ff6ab9400b325e41179dd953d10d056e4f50
funnykoss <noanybody26@gmail.com>
2021-06-18 19:24:10 +0300

    time

--- task_3.js
+++ task_3.js
@@ -2,22 +2,23 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction) => {
+const makeTransaction = (transaction) => {  
     
-  const delay = randomIntegerFromInterval(200, 500);
-return new Promise((resolve, reject) => {
+    
+    return new Promise((resolve, reject) => {
+    const delay = randomIntegerFromInterval(200, 500);
   setTimeout(() => {
     const canProcess = Math.random() > 0.3;
 
     if (canProcess) {
-      resolve(transaction.id, delay);
+        resolve({ id: transaction, time:delay });
     } else {
       reject(transaction.id);
     }
   }, delay);
     })
 };
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
